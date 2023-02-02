038c0940bfa97477930b9474edd7bea93f0b46f4
Camilo <devopsETB@saludcapital.gov.co>
2023-02-02 14:18:18 -0500

    Ajuste VcLugar 250 caracteres

---
+++ Persistencia/Migrations/20230130185451_ChangeLugarSizeToAtencionGrupalTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistencia.Migrations
+{
+    public partial class ChangeLugarSizeToAtencionGrupalTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VcLugar",
+                table: "AtencionGrupal",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VcLugar",
+                table: "AtencionGrupal",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
