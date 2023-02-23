51383b5ef0628ef490998909f7a343ed6521bc7a
osnayder <oconde@soaint.com>
2023-02-23 18:30:55 -0500

    Definiendo DTO de respuesta para consulta de reportes de 'Orientación Web'

---
+++ Dominio/Mapper/AtencionesWeb/AtencionWebReporteDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace Dominio.Mapper.AtencionesWeb
+{
+    public class AtencionWebReporteDTO
+    {
+        public long Id { get; set; }
+        public string Mes { get; set; }
+        public DateTime FechaAlmacenamiento { get; set; }
+        public DateTime FechaOrientacion { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Telefono1 { get; set; }
+        public string Celular { get; set; }
+        public long MotivoId { get; set; }
+        public long SubMotivoId { get; set; }
+        public long CanalAtencionId { get; set; }
+        public long TipoGestionId { get; set; }
+        public long TipoProcesoFallido { get; set; }
+        public string AsuntoCorreoElectronico { get; set; }
+        public string AclaracionMotivoOrientacion { get; set; }
+        public long UsuarioId { get; set; }
+    }
+}
