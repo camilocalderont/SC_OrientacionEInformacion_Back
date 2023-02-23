38aac597c7e40b86b7a3944c89b116499840371a
osnayder <oconde@soaint.com>
2023-02-23 07:49:23 -0500

    Creanción de objeto encargado de almacenar el resultado de operaciones de datos de tipo fecha

---
+++ Dominio/Models/General/DiferenciaEntreFechas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Models.General
+{
+    public class DiferenciaEntreFechas
+    {
+        public int anios = 0;
+        public int meses = 0;
+        public int dias = 0;
+
+        public DiferenciaEntreFechas()
+        {
+
+        }
+
+        public DiferenciaEntreFechas(int anios, int meses, int dias)
+        {
+            this.anios = anios;
+            this.meses = meses;
+            this.dias = dias;
+        }
+
+        public override string ToString()
+        {
+            return anios+" Años, "+meses+" Meses, "+dias+" Dias";
+        }
+
+    }
+}
