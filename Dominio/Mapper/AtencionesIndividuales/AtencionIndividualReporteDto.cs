63b437e05308a88efa2a58f2feb39e482d51a042
osnayder <oconde@soaint.com>
2023-02-24 09:14:01 -0500

    Ajustando el DTO: AtencionIndividualReporteDto.cs. Se agrega un nuevo atributo 'RangoDeEdadAlRegistroDelCaso' y se renombra los nombre de los atributos a notacion Pascal-Case

--- Dominio/Mapper/AtencionesIndividuales/AtencionIndividualReporteDto.cs
+++ Dominio/Mapper/AtencionesIndividuales/AtencionIndividualReporteDto.cs
@@ -1,54 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dominio.Mapper.AtencionesIndividuales
+﻿namespace Dominio.Mapper.AtencionesIndividuales
 {
     public class AtencionIndividualReporteDto
     {
-        public long consecutivo { get; set; }
-        public long cas_Id { get; set; }
-        public int mes { get; set; }
-        public DateTime fechaAlmacenamiento { get; set; }
-        public long tipoDocumentoId { get; set; }
-        public string numeroIdent { get; set; }
-        public string primerApellido { get; set; }
-        public string segundoApellido { get; set; }
-        public string primerNombre { get; set; }
-        public string segundoNombre { get; set; }
-        public string edadAlRegistroDelCaso { get; set; }
-        public string fechaNacimiento { get; set; }
-        public long sexoId { get; set; }
-        public long generoId { get; set; }
-        public long orientacionSexualId { get; set; }
-        public long enfoquePoblacionalId { get; set; }
-        public long poblacionPrioritariaId { get; set; }
-        public long subPoblacionPrioritariaId { get; set; }
-        public long subEtniaId { get; set; }
-        public long etniaId { get; set; }
-        public long regimenId { get; set; }
-        public long aseguradoraId { get; set; }
-        public long nivelSisbenId { get; set; }
-        public long departamentoId { get; set; }
-        public long municipioId { get; set; }
-        public string direccion { get; set; }
-        public long localidadId { get; set; }
-        public long barrioId { get; set; }
-        public long upzId { get; set; }
-        public string  correoElectronico { get; set; }
-        public string telefono1 { get; set; }
+        public string Consecutivo { get; set; }
+        public string Cas_Id { get; set; }
+        public int Mes { get; set; }
+        public string FechaAlmacenamiento { get; set; }
+        public long TipoDocumentoId { get; set; }
+        public string NumeroIdent { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string EdadAlRegistroDelCaso { get; set; }
+        public string RangoDeEdadAlRegistroDelCaso { get; set; }
+        public string FechaNacimiento { get; set; }
+        public long SexoId { get; set; }
+        public long GeneroId { get; set; }
+        public long OrientacionSexualId { get; set; }
+        public long EnfoquePoblacionalId { get; set; }
+        public long PoblacionPrioritariaId { get; set; }
+        public long SubPoblacionPrioritariaId { get; set; }
+        public long SubEtniaId { get; set; }
+        public long EtniaId { get; set; }
+        public long RegimenId { get; set; }
+        public long AseguradoraId { get; set; }
+        public long NivelSisbenId { get; set; }
+        public long DepartamentoId { get; set; }
+        public long MunicipioId { get; set; }
+        public string Direccion { get; set; }
+        public long LocalidadId { get; set; }
+        public long BarrioId { get; set; }
+        public long UpzId { get; set; }
+        public string  CorreoElectronico { get; set; }
+        public string Telefono1 { get; set; }
         public string Telefono2 { get; set; }
-        public string aclaracionesTelfDir { get; set; }
-        public long tipoOrientacion { get; set; }
-        public long motivoId { get; set; }
-        public long subMotivoId { get; set; }
-        public string aclaracionMotivoOrientacion { get; set; }
-        public string gestionResolucionPAcceso { get; set; }
-        public string datosDeContactoAclaración { get; set; }
-        public long canalAtencionId { get; set; }
-        public long estadoCasoId { get; set; }
-        public long usuarioId { get; set; }
+        public string AclaracionesTelfDir { get; set; }
+        public long TipoOrientacion { get; set; }
+        public long MotivoId { get; set; }
+        public long SubMotivoId { get; set; }
+        public string AclaracionMotivoOrientacion { get; set; }
+        public string GestionResolucionPAcceso { get; set; }
+        public string DatosDeContactoAclaración { get; set; }
+        public long CanalAtencionId { get; set; }
+        public long EstadoCasoId { get; set; }
+        public long UsuarioId { get; set; }
     }
 }
